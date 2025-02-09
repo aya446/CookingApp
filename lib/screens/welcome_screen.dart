f57ee18a612755d15f00abf77dfdcf38a1af7483
aya446 <aya.w4474@gmail.com>
2025-02-09 07:01:41 +0200

    add images and fonts, finish welcome page

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,69 @@
+import 'package:cooking_app/screens/Home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:iconify_flutter_plus/iconify_flutter_plus.dart';
+import 'package:iconify_flutter_plus/icons/lucide.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({super.key});
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(
+      const Duration(seconds: 3),
+      () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+              builder: (context) =>
+                  const HomePage()), 
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              'assets/images/main_image.png',
+            ),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Iconify(
+                Lucide.chef_hat,
+                size: 90,
+                color: Colors.white,
+              ).animate().slide(duration: 1.5.seconds, curve: Curves.easeInOut),
+              const Text(
+                'Chef Mate',
+                style: TextStyle(
+                    fontSize: 74, color: Colors.white, fontFamily: 'Jomhuria'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
